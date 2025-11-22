b29b693c4102b2464d2d3ba543a5b280df40ce12
emran-binhasan <imranbinhasan.work@gmail.com>
2025-11-22 20:31:20 +0600

    update font, add shacn ui

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "../globals.css";
+
+
+const montserrat = Montserrat({
+    variable: "--font-montserrat",
+    subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "MegaMart",
+  description: "Your one-stop online shop for everything!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            Welcome to the public home page!
+        </div>
+    );
+}
